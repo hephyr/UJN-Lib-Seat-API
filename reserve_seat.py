b0c6bf30d3e9ea8d48bcc5277e6dfad785b2409e
Zephyr <ixuhefeng@gmail.com>
2017-03-01 15:26:12 +0800

    clean using.txt

--- reserve_seat.py
+++ reserve_seat.py
@@ -21,9 +21,9 @@ def getSeatList():
         seats_str = f.readlines()
     seats = []
     for seat_json in seats_str:
-        data = json.loads(seat)
+        data = json.loads(seat_json)
         p = ujnlib()
-        seat = p.getSeatId(data['room_id'], data['seat_id'])
+        seat = p.getSeatId(data['room_id'], data['seat_num'])
         seats.append(seat)
     return seats
 
@@ -31,6 +31,20 @@ def getSeatList():
 def writeToUsing(username):
     with open('using.txt', 'a') as f:
         f.write(str(username) + '\n')
+
+
+def cleanUsing():
+    with open('using.txt', 'r') as f:
+        using_list = f.readlines()
+    users = [i[:-1] for i in using_list]
+    final_users = []
+    for user in users:
+        p = ujnlib(user)
+        if p.isInUse():
+            final_users.append(user)
+    with open('using.txt', 'w') as f:
+        for user in final_users:
+            f.write(user + '\n')
 
 
 def reserve(t):
@@ -47,6 +61,7 @@ def main():
     times = [(8, 9), (9, 10), (10, 11), (11, 12), (12, 13), (13, 14), (14, 15), (15, 16), (16, 17), (17, 18), (18, 19), (19, 20)]
     for t in times:
         reserve(t)
+    cleanUsing()
 
 
 if __name__ == '__main__':
